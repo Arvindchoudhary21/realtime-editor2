72e7c20a3964fc9a835878dd764de305366b26e0
Arvind Choudhary <arvindchoudhary6290@gmail.com>
2023-11-01 11:08:01 +0530

    final done and working also with avatar

--- src/components/Client.js
+++ src/components/Client.js
@@ -1,9 +1,9 @@
 import React from 'react'
-// import Avatar from 'react-avatar'
+import Avatar from 'react-avatar'
 function Client({ username }) {
     return (
         <div className="client">
-            {/* <Avatar name={username} size={50} round="14px" /> */}
+            <Avatar name={username} size={50} round="14px" />
             <span className="userName">{username}</span>
         </div>
     )
